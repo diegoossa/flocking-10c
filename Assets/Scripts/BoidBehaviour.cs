dd121b8b2bae2edc2aea496a2eba5985dc08b880
Diego Ossa <diegoossa@gmail.com>
2022-09-20 19:24:39 -0500

    Original Project from 10C

---
+++ Assets/Scripts/BoidBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class BoidBehaviour
+{
+	public interface IBoid
+	{
+		Vector3 Position { get; set; }
+		Vector3 Velocity { get; set; }
+	}
+
+	public static void UpdateCoherence(IBoid boid, List<IBoid> neighbours, float coherenceRate, float dt)
+	{
+		if (neighbours.Count > 0)
+		{
+			Vector3 center = neighbours[0].Position;
+			for (int i = 1; i < neighbours.Count; ++i)
+			{
+				center += neighbours[i].Position;
+			}
+			center *= 1.0f / neighbours.Count;
+			boid.Velocity += (center - boid.Position) * coherenceRate * dt;
+		}
+	}
+
+	public static void AvoidOthers(IBoid boid, float minDist, List<IBoid> neighbours, float avoidanceRate, float dt)
+	{
+		if (neighbours.Count > 0)
+		{
+			var myPosition = boid.Position;
+			var minDistSqr = minDist * minDist;
+			Vector3 step = Vector3.zero;
+			for (int i = 0; i < neighbours.Count; ++i)
+			{
+				var delta = myPosition - neighbours[i].Position;
+				var deltaSqr = delta.sqrMagnitude;
+				if (deltaSqr > 0 && deltaSqr < minDistSqr)
+				{
+					step += delta / Mathf.Sqrt(deltaSqr);
+				}
+			}
+			boid.Velocity += step * avoidanceRate * dt;
+		}
+	}
+
+	public static void MatchVelocity(IBoid boid, List<IBoid> neighbours, float matchRate, float dt)
+	{
+		if (neighbours.Count > 0)
+		{
+			Vector3 velocity = Vector3.zero;
+			for (int i = 0; i < neighbours.Count; ++i)
+			{
+				velocity += neighbours[i].Velocity;
+			}
+			velocity /= neighbours.Count;
+			boid.Velocity += (velocity - boid.Velocity) * matchRate * dt;
+		}
+	}
+
+	public static void AvoidInsideBoundsOfCube(IBoid boid, Vector3 halfCubeSize, float avoidRange, float dt)
+	{
+		boid.Velocity -= new Vector3(
+			Mathf.Max(Mathf.Abs(boid.Position.x) - halfCubeSize.x + avoidRange, 0) * Mathf.Sign(boid.Position.x) * 5f * dt,
+			Mathf.Max(Mathf.Abs(boid.Position.y) - halfCubeSize.y + avoidRange, 0) * Mathf.Sign(boid.Position.y) * 5f * dt,
+			Mathf.Max(Mathf.Abs(boid.Position.z) - halfCubeSize.z + avoidRange, 0) * Mathf.Sign(boid.Position.z) * 5f * dt);
+	}
+}
---
+++ Assets/Scripts/GameplayTestMain.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayTestMain : MonoBehaviour 
+{
+	private const float BOID_DENSITY = 4f;
+	private const int ROUND_WORLD_SIZE_TO_MULTIPLES_OF = 5;
+
+	private static uint[] s_boidCounts = new uint[]
+	{
+		64,
+		256,
+		1024,
+		4096,
+	};
+
+	private static readonly BoidSimulator.Team[] s_teams = new BoidSimulator.Team[]
+	{
+		// Red
+		new BoidSimulator.Team
+		{
+			Acceleration = 4,
+			Drag = .02f,
+		},
+
+		// Green
+		new BoidSimulator.Team
+		{
+			Acceleration = 4,
+			Drag = .03f,
+		},
+
+		// Blue
+		new BoidSimulator.Team
+		{
+			Acceleration = 11,
+			Drag = .04f,
+		},
+	};
+
+	[SerializeField]
+	private GameObject[] m_agentPrefabs;
+
+	private BoidSimulator m_boidSimulator = new BoidSimulator();
+	private List<GameObject> m_boidGameObjects = new List<GameObject>();
+
+	private void Start() 
+	{
+		ResetSetup(s_boidCounts[0]);
+		this.enabled = true;
+	}
+
+	private void Update() 
+	{
+		// Reset setup when number keys are pressed
+		for (int i = 0; i < s_boidCounts.Length; ++i) 
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha1 + i)) 
+			{
+				ResetSetup(s_boidCounts[i]);
+				break;
+			}
+		}
+
+		m_boidSimulator.StepSimulation(Time.deltaTime);
+
+		UpdateGameObjects();
+	}
+
+	private void ResetSetup(uint boidCount)
+	{
+		// Destroy all game objects
+		foreach (var gameObject in m_boidGameObjects)
+		{
+			GameObject.Destroy(gameObject);
+		}
+		m_boidGameObjects.Clear();
+
+		// Decide world size based on boid count and density
+		int worldSize = Mathf.CeilToInt(Mathf.Pow(boidCount, 1f / 3) * BOID_DENSITY / ROUND_WORLD_SIZE_TO_MULTIPLES_OF) * ROUND_WORLD_SIZE_TO_MULTIPLES_OF;
+
+		// Reset boid simulator
+		m_boidSimulator.Reset(new Vector3(worldSize, worldSize, worldSize), boidCount, s_teams);
+
+		// Create game objects for the boids
+		for (int boidIndex = 0; boidIndex < m_boidSimulator.Boids.Length; ++boidIndex)
+		{
+			var boid = m_boidSimulator.Boids[boidIndex];
+			var prefab = m_agentPrefabs[boid.Team];
+			var gameObject = GameObject.Instantiate(prefab, boid.Position, Quaternion.identity);
+			m_boidGameObjects.Add(gameObject);
+		}
+	}
+
+	private void UpdateGameObjects()
+	{
+		for (int boidIndex = 0; boidIndex < m_boidSimulator.Boids.Length; ++boidIndex)
+		{
+			var boid = m_boidSimulator.Boids[boidIndex];
+			var gameObject = m_boidGameObjects[boidIndex];
+			gameObject.transform.position = boid.Position;
+			gameObject.transform.LookAt(boid.Position + boid.Velocity);
+		}
+	}
+}
